b116fc9f311eaf7641f6c74050130dc59c341084
CalFo NalDo <devcode940@gmail.com>
2026-08-14 07:26:12 +0300

    refactor: move Java application entry point into active module

---
+++ appJava/src/main/java/com/multaihub/app/MultiAIApp.java
@@ -0,0 +1,9 @@
+package com.multaihub.app;
+
+import android.app.Application;
+
+public final class MultiAIApp extends Application {
+    @Override public void onCreate() {
+        super.onCreate();
+    }
+}
